8f55aeb72bd44f768b67e19949fe9f4ee489d775
katelijacobsen <katjamaehleke98@gmail.com>
2024-09-30 10:01:11 +0200

    exercise

---
+++ js.js
@@ -0,0 +1,10 @@
+let letters = ['a','b','c','d','e','f','g','h',];
+// fremviser det 4. element i array'et.
+console.log(letters[4]);
+
+// laver en ny array, men erstatter elementet 'e' med '*':
+let newArray = letters; 
+
+newArray[4] = '*';
+
+console.log(letters[4]);
