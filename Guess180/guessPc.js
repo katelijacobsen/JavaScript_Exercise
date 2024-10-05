2b609dbe8e64b0af4b8447f3d202ea5f2e034048
katelijacobsen <katjamaehleke98@gmail.com>
2024-10-05 11:50:07 +0200

    push

--- Guess180/guessPc.js
+++ Guess180/guessPc.js
@@ -2,21 +2,45 @@
 //Lad din computer gætte et tal mellem 1-100
 
 //1. Lav variabler der tager fat i input-felterne.
+let startBtn = document.querySelector('#startGame');
 let printGuess = document.querySelector('#printGuess');
-let answer = document.querySelector('#answer');
+let numGuessesEl = document.querySelector('#numGuesses');
+let min = 1;
+let max = 100;
+let guess = 0;
+let numGuesses = 0;
 
-
-// Random number mellem 1-100
+// Random number mellem 1-100/min-max
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
-// funktion med boolsk værdi & if-statement
-function respond(){
-    let guess = randomNumber(1, 100);
-    printGuess.innerHTML = `Is it ${guess}?`; // hawk-tuah, spit on that thing
-    if (guess > 100 || guess < 1) { // eller-operator
+
+function startGame () {
+    makeGuess(); // min = 1, max = 100
+    startBtn.remove();
+}
+
+function makeGuess(){
+    // guess = Math.floor((min + max) / 2); 
+    guess = randomNumber(min, max);  
+    printGuess.innerHTML = `Is it ${guess}?`;
+    numGuesses += 1;
+    numGuessesEl.innerHTML = numGuesses;
+
+}
+
+function handleInput (input){
+    if (input === 'tooHigh'){
+        max = guess - 1;
+        makeGuess(); // min = 1, max = 60
     }
-    else if (guess == answer) {
-        printGuess.innerHTML = 'Nice!';
+    else if (input === 'tooLow'){
+        min = guess + 1;
+        makeGuess(); // min = 1, max = 60
     }
+    else {
+        //korrekt
+        printGuess.innerHTML = "Heck yeah.";
+    }
+    
 }
--- js.js
+++ js.js
@@ -1,10 +1,22 @@
-let letters = ['a','b','c','d','e','f','g','h',];
-// fremviser det 4. element i array'et.
-console.log(letters[4]);
+// const letters = ['a','b','c','d','e','f','g','h',];
+// // fremviser det 4. element i array'et.
+// console.log(letters[4]);
 
-// laver en ny array, men erstatter elementet 'e' med '*':
-let newArray = letters; 
+// // laver en ny array, men erstatter elementet 'e' med '*':
+// const newArray = letters; 
 
-newArray[4] = '*';
+// newArray[4] = '*';
 
-console.log(letters[4]);
+// console.log(letters[4]);
+
+const people = ["Harry", "Ron", "Hermione"];
+let result; 
+
+result = people.push("Draco"); //ttilføjer element til array.
+result = people.push("Neville"); //ttilføjer element til array.
+result = people.push("Luna"); //ttilføjer element til array.
+result = people.pop() // fjerner det sidste element fra arrayet people
+                      // Det fjernede element gemmes i variablen result 
+
+console.log(result);
+console.log(people);
