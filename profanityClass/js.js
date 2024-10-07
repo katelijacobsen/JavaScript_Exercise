f487da70d2cd9b1baac68c270608d4785a0375a0
Katja Mähleke <katjamaehleke98@gmail.com>
2024-10-07 12:59:13 +0200

    push

--- profanityClass/js.js
+++ profanityClass/js.js
@@ -14,10 +14,12 @@ function replaceBadWords() {
   console.log("Hej", theText);
   if (theSentenceHasBeenTreated === false) {
     curseWords.forEach((curseWord) => {
-      theText = theText.replaceAll(curseWord.bad, curseWord.good); // skifter ud med ny tekst
-      document.querySelector("p").textContent = theText;
-      theSentenceHasBeenTreated = true;
+      theText = theText.replaceAll(curseWord.bad, `<span>${curseWord.good}</span>`); // skifter ud med ny tekst
     });
+      document.querySelector("p").innerHTML = theText;
+      theSentenceHasBeenTreated = true;
+      document.querySelector("p").style.color = "red";
+      document.querySelector("p").style.backgroundColor = "yellow";
   }else{
     document.querySelector("dialog").showModal()
   }
